0c4da655e6303ef00d7233b44388bc0eb2e6b097
Soorman <sirtv490@gmail.com>
2026-03-28 18:39:40 +0600

    feat: add NavBar component to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script>
   import '../app.css';
+  import NavBar from '../components/NavBar.svelte';
   let { children } = $props();
 </script>
+
+
+<main>
+<NavBar></NavBar>
+</main>
 
 {@render children()}
