ac75ae8d6ab747e3db3552af1378f6595f8250f2
Faizeen Hoque <haquefaizeen11@gmail.com>
2026-03-28 19:15:26 +0600

    refactor: remove NavBar component and integrate its content directly into layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,41 @@
 <script>
   import '../app.css';
-  import NavBar from '../components/NavBar.svelte';
   let { children } = $props();
 </script>
 
+<main class="flex h-screen">
+  <!-- NavBar -->
+  <div class="text-center bg-neutral-800 px-5 py-5 flex flex-col w-30 gap-8 shrink-0">
+    <div class="font-roboto font-bold text-2xl antialiased text-green-400 text-shadow-lg/30 transition-all hover:animate-ping">F</div>
 
-<main>
-<NavBar></NavBar>
+    <div class="text-gray-500 transition-all duration-300 ease-in-out hover:text-green-400 hover:drop-shadow-green-300 drop-shadow-2xl">
+      <i class="fi-rr-home text-xl text-shadow-lg/30"></i>
+      <p class="font-rubik text-shadow-lg/30">Home</p>
+    </div>
+
+    <div class="text-gray-500 transition-colors ease-in-out hover:text-green-400 block hover:drop-shadow-green-300 drop-shadow-2xl">
+      <i class="fi fi-rr-books-medical text-xl text-shadow-lg/30"></i>
+      <p class="font-rubik text-shadow-lg/30">Library</p>
+    </div>
+
+    <div class="text-gray-500 transition-colors ease-in-out hover:text-green-400 block hover:drop-shadow-green-300 drop-shadow-2xl">
+      <i class="fi fi-rr-document-gavel text-xl text-shadow-lg/30"></i>
+      <p class="font-rubik text-shadow-lg/30">Mods</p>
+    </div>
+
+    <div class="text-gray-500 transition-colors ease-in-out hover:text-green-400 block hover:drop-shadow-green-300 drop-shadow-2xl">
+      <i class="fi fi-rr-user-add text-xl text-shadow-lg/30"></i>
+      <p class="font-rubik text-shadow-lg/30">Accounts</p>
+    </div>
+
+    <div class="text-gray-500 mt-auto transition-colors ease-in-out hover:text-green-400 hover:drop-shadow-green-300 drop-shadow-2xl">
+      <i class="fi fi-rr-settings-sliders text-xl text-shadow-lg/30"></i>
+      <p class="font-rubik text-shadow-lg/30">Settings</p>
+    </div>
+  </div>
+
+  <!-- Page content -->
+  <div class="flex-1 overflow-auto">
+    {@render children()}
+  </div>
 </main>
-
-{@render children()}
--- src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  import NavBar from "../components/NavBar.svelte";
-</script>
-
-<main>
-    <NavBar></NavBar>
-</main>
