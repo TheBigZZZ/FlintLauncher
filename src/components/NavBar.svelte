08c2ea7fd24ac012226cc5a3b80d752ca777e892
Soorman <sirtv490@gmail.com>
2026-03-28 17:01:23 +0600

    feat: implemented NavBar as a component

---
+++ src/components/NavBar.svelte
@@ -0,0 +1,38 @@
+<main>
+    <div class="flex h-screen"> <!--NavBar--->
+        <div class="text-center bg-neutral-800 px-5 py-5 flex flex-col w-30 gap-8">
+        
+            <div class="font-roboto font-bold text-2xl antialiased text-green-400 text-shadow-lg/30 transition-all hover:animate-ping">F</div>
+
+              <!-- Logo -->
+
+            <div class="text-gray-500 transition-all duration-300 ease-in-out hover:text-green-400 hover:drop-shadow-green-300 drop-shadow-2xl">
+                <i class="fi-rr-home text-xl text-shadow-lg/30"></i>
+                <p class="font-rubik text-shadow-lg/30">Home</p>
+            </div>
+
+            <div class="text-gray-500 transition-colors ease-in-out hover:text-green-400 block hover:drop-shadow-green-300 drop-shadow-2xl">
+                <i class="fi fi-rr-books-medical text-xl text-shadow-lg/30"></i>
+                <p class="font-rubik text-shadow-lg/30">Library</p>
+            </div>
+
+            <div class="text-gray-500 transition-colors ease-in-out hover:text-green-400 block hover:drop-shadow-green-300 drop-shadow-2xl">
+                <i class="fi fi-rr-document-gavel text-xl text-shadow-lg/30"></i>
+                <p class="font-rubik text-shadow-lg/30">Mods</p>
+            </div>
+
+
+            <div class="text-gray-500 transition-colors ease-in-out hover:text-green-400 block hover:drop-shadow-green-300 drop-shadow-2xl">
+                <i class="fi fi-rr-user-add text-xl text-shadow-lg/30"></i>
+                <p class="font-rubik text-shadow-lg/30">Accounts</p>
+            </div>
+
+            <div class="text-gray-500 mt-auto transition-colors ease-in-out hover:text-green-400 hover:drop-shadow-green-300 drop-shadow-2xl">
+                <i class="fi fi-rr-settings-sliders text-xl text-shadow-lg/30"></i>
+                <p class="font-rubik text-shadow-lg/30">Settings</p>
+            </div>
+
+
+        </div>
+    </div>
+</main>
